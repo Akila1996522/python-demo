7229e5aa605abde359853d9b30a14687ded5afbb
akila <akila.al24@gmail.com>
2025-06-13 01:51:28 +0530

    fix lint issue

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI
 from app.routes import mailbox, auth, ws, tasks
-from starlette.middleware.cors import CORSMiddleware
+# from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="MailBridge API",
@@ -16,13 +16,13 @@ app = FastAPI(
 #     "https://mailbridge.echonlabs.com/"
 # ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,  
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,  
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # Include API routes
 app.include_router(mailbox.router, prefix="/api/v1/mailbox", tags=["Mailbox"])
