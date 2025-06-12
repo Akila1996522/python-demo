2243dd229822e98129418721cfd444f248935788
akila <akila.al24@gmail.com>
2025-06-13 01:50:10 +0530

    fixing linitin issues

--- app/main.py
+++ app/main.py
@@ -16,13 +16,13 @@ app = FastAPI(
 #     "https://mailbridge.echonlabs.com/"
 # ]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,  
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include API routes
 app.include_router(mailbox.router, prefix="/api/v1/mailbox", tags=["Mailbox"])
